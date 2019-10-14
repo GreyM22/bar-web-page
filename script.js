e71fc9ea599389ee09cb8f2cd172757fb51c842e
GreyM22 <greimuka@gmail.com>
2019-10-14 08:29:23 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+    
+function openNav() {
+    document.getElementByTagName("nav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementByTagName("nav").style.width = "0";
+  }
+</script>
